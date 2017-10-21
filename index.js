79d150529205d7668693da7374c886a1bac3d6f7
Tashan O'Neal <tdoneal91@gmail.com>
2017-10-21 09:21:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,13 @@ function logShout(string) {
 function logWhisper(string) {
   console.log('hello')
 }
+function  sayHiToGrandma(string) {
+  if (string === "hello") {
+    return ("I can\'t hear you!")
+  } else if (string === "HELLO") {
+    return ("YES INDEED!")
+  } else if (string === "I love you, Grandma.") {
+    return ("I love you, too.")
+  }
+}
+
